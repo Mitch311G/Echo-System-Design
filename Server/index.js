9a4d96cfb514538f9e56a2511aae24fe4b2b2f11
Mitchell <mitchellwgardner@gmail.com>
2022-01-13 16:42:38 -0700

    API endpoints created

--- Server/index.js
+++ Server/index.js
@@ -7,11 +7,12 @@ const PORT = 3000;
 app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)});
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // API End-points //
 
 // get request to reviews
-app.get('/api/reviews/*', (req, res) => {
+app.get('/api/reviews/', (req, res) => {
   let queryStirng = 'SELECT * FROM reviews WHERE product_id = 44388';
   let queryArgs = [];
   db.query(queryStirng, queryArgs, (err, results) => {
@@ -29,16 +30,16 @@ app.post('/api/reviews', (req, res) => {
 })
 
 // get request to reviews meta
-app.get('/api/reviews/meta/*', (req, res) => {
+app.get('/api/reviews/meta', (req, res) => {
   //query DB
 })
 
 // put request to update helpful
-app.put('/api/reviews/*/helpful', (req, res) => {
+app.put('/api/reviews/:review_id/helpful', (req, res) => {
   //query DB
 })
 
 // put request to report review
-app.put('/api/reviews/*/report', (req, res) => {
+app.put('/api/reviews/:review_id/report', (req, res) => {
   //query DB
 })
