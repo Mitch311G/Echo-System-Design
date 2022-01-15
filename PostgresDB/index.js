906d25e6fe11c6f4782f010f3a91e652b081ccdc
Mitchell <mitchellwgardner@gmail.com>
2022-01-15 11:31:07 -0700

    Created post reviews request, not tested

--- PostgresDB/index.js
+++ PostgresDB/index.js
@@ -16,7 +16,8 @@ pool.connect((err) => {
   }
 });
 
-async function getReviews (req, res) {
+// GET REVIEWS FUNCTION
+const getReviews = (req, res) => {
   const page = Number(req.query.page) || 1;
   const count = Number(req.query.count) || 5;
   const sort = req.query.sort || 'relevant';
@@ -28,40 +29,66 @@ async function getReviews (req, res) {
     count: count,
   };
 
-  let reviews;
-
   async function getPhotos (reviews) {
     for(let review of reviews) {
       const id = review.review_id;
-      const queryStirng2 = 'SELECT id, url FROM photos WHERE review_id=$1';
-      const queryArgs2 = [id]
-      await pool.query(queryStirng2, queryArgs2)
+      const queryPhotoStirng = 'SELECT id, url FROM photos WHERE review_id=$1';
+      const queryPhotoArgs = [id]
+      await pool.query(queryPhotoStirng, queryPhotoArgs)
         .then(results => review['photos'] = results.rows)
+        .catch(err => res.status(400).send(err))
     }
     return reviews
   }
 
-  const queryStirng = 'SELECT * FROM reviews WHERE product_id=$1 LIMIT $2';
-  const queryArgs = [product_id, count];
-  await pool.query(queryStirng, queryArgs)
+  const queryReviewStirng = 'SELECT * FROM reviews WHERE product_id=$1 LIMIT $2';
+  const queryReviewArgs = [product_id, count];
+  pool.query(queryReviewStirng, queryReviewArgs)
     .then(results => getPhotos(results.rows))
     .then(reviews => response['results'] = reviews)
     .then(() => res.status(200).send(response))
-};
+    .catch(err => res.status(400).send(err))
+  };
 
-const postReview = (req, res) => {
-  const {} = req.body;
-  const queryStirng = ``;
-  const queryArgs = [];
-  pool.query(queryStirng, queryArgs, (err, results) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(201);
+// POST NEW REVIEW FUNCTION
+ const postReview = (req, res) => {
+  const { product_id, rating, summary, body, recommend, name, email, photos, characteristics } = req.body;
+  const date = Date.now();
+
+  async function postPhotos (review_id) {
+    if (photos.length === 0) {return}
+
+    for (let url of photos) {
+      const queryPhotoStirng = 'INSERT INTO photos(review_id, url) VALUES ($1, $2)'
+      const queryPhotoArgs = [review_id, url]
+      await pool.query(queryPhotoStirng, queryPhotoArgs)
+        .catch(err => res.status(400))
     }
-  });
+  }
+
+  async function postChars (review_id) {
+    for (let characteristic_id in characteristics) {
+      let value = characteristics[characteristic_id]
+
+      const queryCharString = 'INSERT INTO characteristicReviews(characteristic_id, review_id, value) VALUES($1, $2, $3)';
+      const queryCharArgs = [characteristic_id, review_id, value]
+      await pool.query(queryCharString, queryCharArgs)
+        .catch(err => res.status(400))
+    }
+  }
+
+  const queryReviewStirng = 'INSERT INTO reviews(product_id, rating, date, summary, body, recommend, reviewer_name, reviewwer_email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING review_id';
+  const queryReviewArgs = [product_id, rating, date, summary, body, recommend, name, email];
+  pool.query(queryReviewStirng, queryReviewArgs)
+    .then(review_id => {
+      postPhotos(review_id);
+      postChars(review_id);
+    })
+    .then(() => res.status(204))
+    .catch(err => res.status(400))
 };
 
+// GET REVEIW META DATA FUNCTION
 const getReviewMeta = (req, res) => {
   const product_id = req.query.product_id;
   let response = {
@@ -70,41 +97,29 @@ const getReviewMeta = (req, res) => {
 
   const queryStirng = ``;
   const queryArgs = [];
-  pool.query(queryStirng, queryArgs, (err, results) => {
-    if (err) {
-      res.status(400).send(err)
-    } else {
-      res.status(200).send(results)
-    }
-  });
+  pool.query(queryStirng, queryArgs)
 };
 
+// UPDATE HELPFUL FUNCTION
 const updateHelpful = (req, res) => {
   const review_id = req.params.review_id
 
   const queryStirng = 'UPDATE reviews SET helpfulness=helpfulness+1 WHERE review_id=$1';
   const queryArgs = [review_id];
-  pool.query(queryStirng, queryArgs, (err, results) => {
-    if (err) {
-      res.status(400).send(err)
-    } else {
-      res.status(204)
-    }
-  });
+  pool.query(queryStirng, queryArgs)
+    .then(() => res.status(204))
+    .catch(err => res.status(400).send(err))
 };
 
+// UPDATE REPORT FUNCTION
 const updateReport = (req, res) => {
   const review_id = req.params.review_id
 
   const queryStirng = 'UPDATE reviews SET reported=true WHERE review_id=$1 ';
   const queryArgs = [review_id];
-  pool.query(queryStirng, queryArgs, (err, results) => {
-    if (err) {
-      res.status(400).send(err)
-    } else {
-      res.status(204)
-    }
-  });
+  pool.query(queryStirng, queryArgs)
+    .then(() => res.status(204))
+    .catch(err => res.status(400).send(err))
 };
 
 module.exports = {
